61bf10bf31709c8207a86bce43a11933d10ed922
AnishKelkar1 <kelkaranish1@gmail.com>
2024-07-22 11:13:41 -0400

    testing

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://AnishKelkar1.github.io";
+const repoURL = "https://github.com/AnishKelkar1/AnishKelkar1.github.io";
 
 ghpages.publish(
   pathname,
